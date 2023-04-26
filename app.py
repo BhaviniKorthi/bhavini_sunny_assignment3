c93c1fbacd8848d4b99e3175e2d8b906c8aa26f4
BhaviniKorthi <76489649+BhaviniKorthi@users.noreply.github.com>
2023-04-26 13:56:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,25 @@ from PIL import Image
 import os
 
 
-st.header("Latent Space Visualisation")
-st.sidebar.markdown("Here's some additional information about the visualisation:")
-
-
-
+st.sidebar.title("Navigation")
+page = st.sidebar.selectbox("Select a page", ["Slider", "Reconstructed Images"])
+st.sidebar.markdown("   ")
 files = os.listdir("plots")
 num_plots = len(files)
 image_dict = {}
 for i in range(num_plots):
     index = (i+1)*5
     image_dict[index] = Image.open(f"plots/plot{i+1}.png")
-selected_index = st.slider("Epochs", 5, num_plots*5, 5, step=5)
-st.image(image_dict[selected_index], caption=f"Plot {selected_index//5}", use_column_width=True)
+
+if page == "Slider":
+    st.title("Latent Space Visualisation")
+    for i in range(num_plots):
+        index = (i+1)*5
+        image_dict[index] = Image.open(f"plots/plot{i+1}.png")
+    selected_index = st.slider("Epochs", 5, num_plots*5, 5, step=5)
+    st.image(image_dict[selected_index], caption=f"Plot {selected_index//5}", use_column_width=True)
+else:
+    st.title("Reconstructed Images")
+    st.image(Image.open("Reconstructed/Figure_1.png"), caption="PlotX", use_column_width=True, width=300)
+
+
