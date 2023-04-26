4119a958b61cb3f84acbb132dec1feb42c7c5405
BhaviniKorthi <76489649+BhaviniKorthi@users.noreply.github.com>
2023-04-26 06:52:32 +0530

    Update latent_space_visualization.py

--- latent_space_visualization.py
+++ latent_space_visualization.py
@@ -42,9 +42,9 @@ transform = transforms.Compose([
     transforms.ToTensor(), 
     transforms.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784))]
 )
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform)
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform)
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
 classes = testset.classes
 
